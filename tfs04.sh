d0c120ddba0fd2ee16c9854147d564dad193019b
ivens Pontes <ivenscardoso@hotmail.com>
2019-08-07 23:48:39 -0300

    add packages of tfs 0.4

---
+++ tfs04.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+sudo apt update
+
+sudo apt install autoconf automake libxml2-dev build-essential libboost-dev zlib1g-dev libgmp3-dev libssl-dev libboost-thread-dev libboost-regex-dev libboost-filesystem-dev liblua5.1-0-dev libmysqlclient-dev
+
