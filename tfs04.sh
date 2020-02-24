92c5dceccab4c7c09112e3438bca4d45a3a52ba2
Ivens Pontes <ivenscardoso@hotmail.com>
2020-02-24 14:54:12 -0300

    Update tfs04.sh
    fix xml2 version error

--- tfs04.sh
+++ tfs04.sh
@@ -2,5 +2,5 @@
 
 sudo apt update
 
-sudo apt install autoconf automake libxml2-dev build-essential libboost-dev zlib1g-dev libgmp3-dev libssl-dev libboost-thread-dev libboost-regex-dev libboost-filesystem-dev liblua5.1-0-dev libmysqlclient-dev
+sudo apt install autoconf automake libxml2-dev build-essential libboost-dev zlib1g-dev libgmp3-dev libssl-dev libboost-thread-dev libboost-regex-dev libboost-filesystem-dev liblua5.1-0-dev libmysqlclient-dev liblzma-dev
 
